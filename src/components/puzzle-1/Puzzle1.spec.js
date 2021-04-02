ef0744be5a69aa2445771ed44a0a5915dba7d5a4
jmarti <jmarti@theinit.com>
2021-04-02 19:31:44 +0200

    puzzle 1 - wip - invert letters with handle a

--- src/components/puzzle-1/Puzzle1.spec.js
+++ src/components/puzzle-1/Puzzle1.spec.js
@@ -1,5 +1,38 @@
+import { shallowMount } from '@vue/test-utils';
+import Puzzle1 from './Puzzle1';
+
 describe('Puzzle 1', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+  it('changes with handle a', () => {
+    const puzzle1 = shallowMount(Puzzle1);
+    puzzle1.vm.status = ['XXX', 'XOX', 'OXX'];
+
+    puzzle1.find('.handle-a').trigger('click');
+
+    expect(puzzle1.vm.status).toEqual([
+      'OOO', 'XOX', 'OXX'
+    ]);
   });
+
+  it('changes with handle a (case b)', () => {
+    const puzzle1 = shallowMount(Puzzle1);
+    puzzle1.vm.status = ['XOX', 'XOX', 'OXX'];
+
+    puzzle1.find('.handle-a').trigger('click');
+
+    expect(puzzle1.vm.status).toEqual([
+      'OXO', 'XOX', 'OXX'
+    ]);
+  })
+
+  xit('changes with handle b', () => {
+    const puzzle1 = shallowMount(Puzzle1);
+    puzzle1.vm.status = ['XOX', 'XXO', 'OXX'];
+
+    puzzle1.find('.handle-b').trigger('click');
+
+    expect(puzzle1.vm.status).toEqual([
+      'OXO', 'XXO', 'OXX'
+    ]);
+  })
+
 });
--- src/components/puzzle-1/Puzzle1.vue
+++ src/components/puzzle-1/Puzzle1.vue
@@ -1,9 +1,63 @@
 <template>
-  <div>hola, vengo del puzle1</div>
+  <div>
+    <div class="panel">
+      <div class="cell"></div>
+      <div class="cell handle handle-1">-</div>
+      <div class="cell handle handle-2">-</div>
+      <div class="cell handle handle-3">-</div>
+      <div class="cell handle handle-a" @click="clickHandle('a')">-</div>
+      <div class="cell">X</div>
+      <div class="cell">X</div>
+      <div class="cell">X</div>
+      <div class="cell handle handle-b">-</div>
+      <div class="cell">X</div>
+      <div class="cell success">O</div>
+      <div class="cell">X</div>
+      <div class="cell handle handle-c">-</div>
+      <div class="cell success">O</div>
+      <div class="cell">X</div>
+      <div class="cell">X</div>
+    </div>
+  </div>
 </template>
+<style>
+.panel {
+  border: solid 1px #999;
+  width: 60vw;
+  height: 70vh;
+  margin-top: 8vh;
+  margin-left: 18vw;
+  display:flex;
+  flex-wrap: wrap;
+}
+.panel .cell {
+  min-width: 25%;
+  text-align: center;
+  outline: solid 1px #900;
+  font-size: 7vh;
+  padding-top: 5vh;
+}
 
+.panel .success {
+  background-color: rgba(00, 99, 00, 0.2);
+}
+</style>
 <script>
+
+const invertLetter = letter => letter === 'X' ? 'O' : 'X';
+const invert = (str) => str.split('').map(invertLetter).join('');
+
 export default {
   name: 'puzzle-1',
+  data () {
+    return {
+      status: ['', '', ''],
+    };
+  },
+  methods: {
+    clickHandle(_) {
+      this.status[0] = invert(this.status[0]);
+    },
+  },
 };
 </script>
