a7ac95b1e573bdee189c508bd2d190a31301f9c2
jmarti <jmarti@theinit.com>
2021-04-03 09:54:55 +0200

    invert rows and cols in puzzle 1

--- src/components/puzzle-1/Puzzle1.spec.js
+++ src/components/puzzle-1/Puzzle1.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils';
 import Puzzle1 from './Puzzle1';
 
 describe('Puzzle 1', () => {
-  it('changes with handle a', () => {
+  it('changes with handle row1', () => {
     const puzzle1 = shallowMount(Puzzle1);
     puzzle1.vm.status = ['XXX', 'XOX', 'OXX'];
 
@@ -13,7 +13,7 @@ describe('Puzzle 1', () => {
     ]);
   });
 
-  it('changes with handle a (case b)', () => {
+  it('changes with handle row1 (case b)', () => {
     const puzzle1 = shallowMount(Puzzle1);
     puzzle1.vm.status = ['XOX', 'XOX', 'OXX'];
 
@@ -24,15 +24,58 @@ describe('Puzzle 1', () => {
     ]);
   })
 
-  xit('changes with handle b', () => {
+  it('changes with handle row2', () => {
     const puzzle1 = shallowMount(Puzzle1);
     puzzle1.vm.status = ['XOX', 'XXO', 'OXX'];
 
     puzzle1.find('.handle-b').trigger('click');
 
     expect(puzzle1.vm.status).toEqual([
-      'OXO', 'XXO', 'OXX'
+      'XOX', 'OOX', 'OXX'
     ]);
-  })
+  });
 
+  it('changes with handle row3', () => {
+    const puzzle1 = shallowMount(Puzzle1);
+    puzzle1.vm.status = ['XOX', 'XXO', 'OXX'];
+
+    puzzle1.find('.handle-c').trigger('click');
+
+    expect(puzzle1.vm.status).toEqual([
+      'XOX', 'XXO', 'XOO'
+    ]);
+  });
+
+  it('changes with handle col1', () => {
+    const puzzle1 = shallowMount(Puzzle1);
+    puzzle1.vm.status = ['XOX', 'XXO', 'OXX'];
+
+    puzzle1.find('.handle-1').trigger('click');
+
+    expect(puzzle1.vm.status).toEqual([
+      'OOX', 'OXO', 'XXX'
+    ]);
+  });
+
+  it('changes with handle col2', () => {
+    const puzzle1 = shallowMount(Puzzle1);
+    puzzle1.vm.status = ['XOX', 'XXO', 'OXX'];
+
+    puzzle1.find('.handle-2').trigger('click');
+
+    expect(puzzle1.vm.status).toEqual([
+      'XXX', 'XOO', 'OOX'
+    ]);
+  });
+
+  it('changes with handle col3', () => {
+    const puzzle1 = shallowMount(Puzzle1);
+    puzzle1.vm.status = ['XOX', 'XXO', 'OXX'];
+
+    puzzle1.find('.handle-3').trigger('click');
+
+    expect(puzzle1.vm.status).toEqual([
+      'XOO', 'XXX', 'OXO'
+    ]);
+  });
 });
--- src/components/puzzle-1/Puzzle1.vue
+++ src/components/puzzle-1/Puzzle1.vue
@@ -2,18 +2,18 @@
   <div>
     <div class="panel">
       <div class="cell"></div>
-      <div class="cell handle handle-1">-</div>
-      <div class="cell handle handle-2">-</div>
-      <div class="cell handle handle-3">-</div>
-      <div class="cell handle handle-a" @click="clickHandle('a')">-</div>
+      <div class="cell handle handle-1" @click="clickHandle('col1')">-</div>
+      <div class="cell handle handle-2" @click="clickHandle('col2')">-</div>
+      <div class="cell handle handle-3" @click="clickHandle('col3')">-</div>
+      <div class="cell handle handle-a" @click="clickHandle('row1')">-</div>
       <div class="cell">X</div>
       <div class="cell">X</div>
       <div class="cell">X</div>
-      <div class="cell handle handle-b">-</div>
+      <div class="cell handle handle-b" @click="clickHandle('row2')">-</div>
       <div class="cell">X</div>
       <div class="cell success">O</div>
       <div class="cell">X</div>
-      <div class="cell handle handle-c">-</div>
+      <div class="cell handle handle-c" @click="clickHandle('row3')">-</div>
       <div class="cell success">O</div>
       <div class="cell">X</div>
       <div class="cell">X</div>
@@ -45,18 +45,38 @@
 <script>
 
 const invertLetter = letter => letter === 'X' ? 'O' : 'X';
-const invert = (str) => str.split('').map(invertLetter).join('');
+
+const invertRow = (str) => str.split('').map(invertLetter).join('');
+const invertOnlyOneRow = (numRow) => (row, idx) => idx === (numRow - 1) ? invertRow(row) : row;
+const invertByRow = (status, numRow) => status.map(invertOnlyOneRow(numRow));
+
+const invertOnlyOneLetter = numCol =>  (letter, idx) => idx === (numCol-1) ? invertLetter(letter) : letter;
+const invertOneColInRow = numCol => (row) => row.split('').map(invertOnlyOneLetter(numCol)).join('')
+const invertByCol = (status, numCol) => status.map(invertOneColInRow(numCol));
+
 
 export default {
   name: 'puzzle-1',
   data () {
     return {
-      status: ['', '', ''],
+      status: ['XXX', 'XXX', 'XXX'],
     };
   },
   methods: {
-    clickHandle(_) {
-      this.status[0] = invert(this.status[0]);
+    clickHandle(handle) {
+      if (handle === 'row1') {
+        this.status = invertByRow(this.status, 1);
+      } else if (handle === 'row2') {
+        this.status = invertByRow(this.status, 2);
+      } else if (handle === 'row3') {
+        this.status = invertByRow(this.status, 3);
+      } else if (handle === 'col1') {
+        this.status = invertByCol(this.status, 1);
+      } else if (handle === 'col2') {
+        this.status = invertByCol(this.status, 2);
+      } else if (handle === 'col3') {
+        this.status = invertByCol(this.status, 3);
+      }
     },
   },
 };
