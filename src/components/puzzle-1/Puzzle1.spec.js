43914b97a1c2ae0d99e3514378a1f3db4e00b711
jmarti <jmarti@theinit.com>
2021-04-05 17:46:44 +0200

    get puzzle 1 stage based on the number of player i am

--- src/components/puzzle-1/Puzzle1.spec.js
+++ src/components/puzzle-1/Puzzle1.spec.js
@@ -4,9 +4,14 @@ import { shallowMount } from '@vue/test-utils';
 import Puzzle1 from './Puzzle1';
 import Puzzle1Stage from './Puzzle1Stage';
 import firebaseUtil from '../../lib/firebase/firebase-util';
+import GetNumPlayer from '../../lib/get-num-player.js';
+
+Vue.use(MockVueFire);
 
 describe('Puzzle 1', () => {
   it('shows the EASY (3 transistors) puzzle stage', () => {
+    jest.mock('@/lib/get-num-player.js');
+    GetNumPlayer.get = jest.fn(() => 1);
     const puzzle1 = shallowMount(Puzzle1);
 
     const theStage = puzzle1.findComponent(Puzzle1Stage);
@@ -17,8 +22,24 @@ describe('Puzzle 1', () => {
   });
 
   it('shows the MEDIUM (4 transistors + monoplayer) puzzle stage', () => {
+    jest.mock('@/lib/get-num-player.js');
+    GetNumPlayer.get = jest.fn(() => 1);
     firebaseUtil.doc.mockImplementation(() => ({ 'stagePlayer1': 2 }));
-    Vue.use(MockVueFire);
+
+    const puzzle1 = shallowMount(Puzzle1);
+
+    const theStage = puzzle1.findComponent(Puzzle1Stage);
+
+    expect(theStage.vm.initialStatus).toEqual([
+      'XOXO', 'OXOX', 'XOXO', 'OXOX',
+    ]);
+  });
+
+  it('shows the MEDIUM puzzle stage, if player 2 is in stage 2', () => {
+    jest.mock('@/lib/get-num-player.js');
+    GetNumPlayer.get = jest.fn(() => 2);
+    firebaseUtil.doc.mockImplementation(() => ({ 'stagePlayer2': 2 }));
+
     const puzzle1 = shallowMount(Puzzle1);
 
     const theStage = puzzle1.findComponent(Puzzle1Stage);
--- src/components/puzzle-1/Puzzle1.vue
+++ src/components/puzzle-1/Puzzle1.vue
@@ -1,25 +1,28 @@
 <template>
   <div>
-    <puzzle-1-stage :initial-status="solveStatus"/>
+    <puzzle-1-stage v-if="myStage === 1" :initial-status="['XOO', 'OXX', 'XOO']"/>
+    <puzzle-1-stage v-if="myStage === 2" :initial-status="['XOXO', 'OXOX', 'XOXO', 'OXOX']"/>
   </div>
 </template>
 <script>
 import Puzzle1Stage from './Puzzle1Stage';
 import firebaseUtil from '../../lib/firebase/firebase-util';
+import GetNumPlayer from '../../lib/get-num-player';
 
 export default {
   name: 'Puzzle1',
   components: { Puzzle1Stage, },
   computed: {
-    solveStatus () {
-      return this.puzzleStatus.stagePlayer1 === 2 ?
-          ['XOXO', 'OXOX', 'XOXO', 'OXOX'] :
-          ['XOO', 'OXX', 'XOO'];
+    myStage () {
+      const numPlayer = GetNumPlayer.get();
+      if (this.puzzleStatus.notloaded) return null;
+      const theStage =  this.puzzleStatus[`stagePlayer${numPlayer}`];
+      return theStage ? theStage : 1;
     },
   },
   data() {
     return {
-      puzzleStatus: { stagePlayer1: 1 },
+      puzzleStatus: { notloaded: true },
     };
   },
   firestore() {
