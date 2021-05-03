8360c317cea2c1e248e6f98092aa83988325d8ba
jmarti <jmarti@theinit.com>
2021-05-03 19:30:08 +0200

    show files on file explorer (albertsarle & cocodibuja session)

--- src/components/file-explorer/FileExplorer.spec.js
+++ src/components/file-explorer/FileExplorer.spec.js
@@ -1,10 +1,55 @@
 import FileExplorer from './FileExplorer';
 import { mount } from '@vue/test-utils';
+import { givenPlayerNumber } from '../../test-utils/game-test-utils';
 
 describe('File Explorer', () => {
-  it('should mount', () => {
+  it('shows 2 files when opening', () => {
     const fileExplorer = mount(FileExplorer);
 
-    expect(fileExplorer).not.toBeUndefined();
+    const files = fileExplorer.findAll('[data-test-id=file]');
+    expect(files.length).toBe(2);
+    expect(files.at(0).text()).toMatch('files.painting')
+  });
+
+  it('shows chess player 1 file when player 1', () => {
+    givenPlayerNumber(1);
+
+    const fileExplorer = mount(FileExplorer);
+
+    const files = fileExplorer.findAll('[data-test-id=file]');
+    expect(files.at(1).text()).toMatch('files.chess-player-1')
+  });
+
+  it('shows chess player 2 file when player 2', () => {
+    givenPlayerNumber(2);
+
+    const fileExplorer = mount(FileExplorer);
+
+    const files = fileExplorer.findAll('[data-test-id=file]');
+    expect(files.at(1).text()).toMatch('files.chess-player-2')
+  });
+
+  describe('Shows files bigger when file is selected', () => {
+    it('no image is shown when no files are selected', () => {
+      givenPlayerNumber(1);
+
+      const fileExplorer = mount(FileExplorer);
+
+      expect(fileExplorer.find('[data-test-id=zoom-image]').exists()).toBeFalsy();
+    });
+
+    it.each`
+      playerNumber | expectedSrc
+      ${1}         | ${'chess-player1.jpg'}
+      ${2}         | ${'chess-player2.jpg'}
+    `('image is shown when a file is selected - playerNumber: $playerNumber', async ( { playerNumber, expectedSrc } ) => {
+      givenPlayerNumber(playerNumber);
+      const fileExplorer = mount(FileExplorer);
+
+      fileExplorer.findAll('[data-test-id=file]').at(1).trigger('click');
+      await fileExplorer.vm.$nextTick();
+
+      expect(fileExplorer.find('[data-test-id=zoom-image]').element.getAttribute("src")).toMatch(expectedSrc);
+    });
   });
 });
--- src/components/file-explorer/FileExplorer.vue
+++ src/components/file-explorer/FileExplorer.vue
@@ -1,8 +1,26 @@
 <template>
-  <div>esto es el file explorer</div>
+  <div>
+    <div data-test-id="file">{{ $t('files.painting') }}</div>
+    <div v-if="playerNumber === 1" data-test-id="file" @click="selectFile('chess-player1.jpg')">{{ $t('files.chess-player-1') }}</div>
+    <div v-if="playerNumber === 2" data-test-id="file" @click="selectFile('chess-player2.jpg')">{{ $t('files.chess-player-2') }}</div>
+    <img v-if="selectedFile" data-test-id="zoom-image" alt="image" :src="selectedFile">
+  </div>
 </template>
 <script>
+import GetNumPlayer from '../../lib/get-num-player';
+
 export default {
-  name: 'FileExplorer'
+  name: 'FileExplorer',
+  data() {
+    return {
+      playerNumber: GetNumPlayer.get(),
+      selectedFile: null
+    }
+  },
+  methods: {
+    selectFile(file) {
+      this.selectedFile = `./files/${file}`;
+    }
+  }
 }
 </script>
