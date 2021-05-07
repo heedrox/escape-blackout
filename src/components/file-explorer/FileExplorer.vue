d7192f682098aed7d8c8954f5bcf4f9379e7534e
jmarti <jmarti@theinit.com>
2021-05-07 18:55:40 +0200

    style image in file explorer

--- src/components/file-explorer/FileExplorer.vue
+++ src/components/file-explorer/FileExplorer.vue
@@ -1,21 +1,37 @@
 <template>
-  <div>
-    <div class="files">
+  <div class="file-explorer">
+    <div class="files-left">
       <div :class="{ 'file': true, 'selected': isSelected('cuadro-abstracto.jpg') }"  data-test-id="file" @click="selectFile('cuadro-abstracto.jpg')">{{ $t('files.painting') }}</div>
       <div v-if="playerNumber === 1" :class="{ 'file': true, 'selected': isSelected('chess-player1.jpg') }" data-test-id="file" @click="selectFile('chess-player1.jpg')">{{ $t('files.chess-player-1') }}</div>
       <div v-if="playerNumber === 2" :class="{ 'file': true, 'selected': isSelected('chess-player2.jpg') }" data-test-id="file" @click="selectFile('chess-player2.jpg')">{{ $t('files.chess-player-2') }}</div>
     </div>
-    <img v-if="selectedFile" data-test-id="zoom-image" alt="image" :src="fullSelectedFile">
+    <div class="file-zoom">
+      <img v-if="selectedFile" data-test-id="zoom-image" alt="image" :src="fullSelectedFile">
+    </div>
   </div>
 </template>
 <style scoped="true">
-.files {
+.file-explorer {
+  display: flex;
+  height: 100%;
+  align-items: stretch;
+}
+.files-left {
   padding: 2vh 1vw;
   width: 35vw;
   border-right: solid 1px #999999;
   height: 82.5vh;
   overflow-y: visible;
   overflow-x: hidden;
+}
+
+.file-zoom {
+  width: 100%;
+  height: 86.5vh;
+  border: solid 1px #999999;
+  display: flex;
+  justify-content: center;
+  align-content: center;
 }
 
 .file {
@@ -29,6 +45,13 @@
   background-color: #999999;
   color: #000;
 }
+
+.file-zoom img {
+  width: 90%;
+  height: fit-content;
+  max-height: 86vh;
+  align-self: center;
+}
 </style>
 <script>
 import GetNumPlayer from '../../lib/get-num-player';
@@ -37,7 +60,7 @@ export default {
   name: 'FileExplorer',
   computed: {
     fullSelectedFile() {
-      return `./files/${this.selectedFile}`;
+      return require(`./files/${this.selectedFile}`);
     }
   },
   data() {
@@ -52,7 +75,7 @@ export default {
     },
     isSelected(file) {
       return this.selectedFile === file;
-    }
+    },
   }
 }
 </script>
--- src/test-utils/setup.js
+++ src/test-utils/setup.js
@@ -2,6 +2,16 @@ import Vue from 'vue';
 import { config } from '@vue/test-utils'
 import MockVueFire from './MockVueFire';
 
+const IMAGES_TO_MOCK = [
+  '@/components/file-explorer/files/chess-player1.jpg',
+  '@/components/file-explorer/files/chess-player2.jpg',
+  '@/components/file-explorer/files/cuadro-abstracto.jpg',
+];
+
+IMAGES_TO_MOCK.forEach((mockImage) => {
+  jest.mock(mockImage, () => mockImage);
+});
+
 jest.mock('@/lib/firebase/firebase-util.js', () => ({
   doc: jest.fn(() => ({ mocked: true })),
   collection: jest.fn( () => [ { mocked: true }]),
